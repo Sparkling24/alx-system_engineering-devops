01f3cf1b08e730c04c595bee68795a33268fb6f6
Sparkling24 <111319163+Sparkling24@users.noreply.github.com>
2024-06-28 21:55:10 +0100

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
 #!/usr/bin/pup
-package { 'puppet-lint':
-  ensure   => '2.5.0',
-  provider => 'gem',
+# Install an especific version of flask (2.1.0)
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
