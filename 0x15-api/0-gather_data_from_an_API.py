1e297437e633e362d4019d89330a68e1039060c1
Sparkling24 <chimexdguy2020@gmail.com>
2024-08-01 00:40:51 -0700

    execupdt

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,8 +3,6 @@
 import requests
 import sys
 
-(python3 -c 'print(__import__("my_module").__doc__)')
-
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
     user = requests.get(url + "users/{}".format(sys.argv[1])).json()
